a5b13a0ebbec1d310933da327067b1d6a4250a8b
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2024-03-26 05:15:48 +0700

    добавить хоть какие-то стили для выбора профессии

--- src/components/ProfPopup/ProfPopup.tsx
+++ src/components/ProfPopup/ProfPopup.tsx
@@ -11,29 +11,33 @@ type TProfPopup = {
 export function ProfPopup({ showPopup, onChange }: TProfPopup) {
   return (
     <PopupPortal isOpen={showPopup}>
-      <div className={styles.wrapper}>
-        <fieldset
-          onChange={(event: React.FormEvent<HTMLFieldSetElement>) => {
-            const target = event.target as HTMLInputElement
-            if (target && target.type === "radio") {
-              const selectedProf = prof.find(
-                prof => prof.id.toString() === target.id,
-              )
-              if (selectedProf) {
-                onChange(selectedProf)
-                console.log(selectedProf)
+      
+      <div className={styles["popup"]}>
+        <div className={styles['card']}>
+          <fieldset
+            className={styles["fieldset"]}
+            onChange={(event: React.FormEvent<HTMLFieldSetElement>) => {
+              const target = event.target as HTMLInputElement
+              if (target && target.type === "radio") {
+                const selectedProf = prof.find(
+                  prof => prof.id.toString() === target.id,
+                )
+                if (selectedProf) {
+                  onChange(selectedProf)
+                  console.log(selectedProf)
+                }
               }
-            }
-          }}
-        >
-          <legend>Укажите профессию</legend>
-          {prof.map(prof => (
-            <div key={prof.id}>
-              <input type="radio" id={`${prof.id}`} name="profession" />
-              <label htmlFor={`${prof.id}`}>{prof.prof_name}</label>
-            </div>
-          ))}
-        </fieldset>
+            }}
+          >
+            <legend>Укажите профессию</legend>
+            {prof.map(prof => (
+              <div key={prof.id}>
+                <input type="radio" id={`${prof.id}`} name="profession" />
+                <label htmlFor={`${prof.id}`}>{prof.prof_name}</label>
+              </div>
+            ))}
+          </fieldset>
+        </div>
       </div>
     </PopupPortal>
   )
