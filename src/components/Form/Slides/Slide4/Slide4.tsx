7c56189e04637902ece3dff08a8ed450cdd00cbb
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2024-03-25 12:25:51 +0700

    сделать слайд 4 по макету

--- src/components/Form/Slides/Slide4/Slide4.tsx
+++ src/components/Form/Slides/Slide4/Slide4.tsx
@@ -1,5 +1,4 @@
 import { FormLayout } from "@/components/FormLayout/FormLayout"
-import globalStyle from "@/globalStyle.module.css"
 import { type ZSlide4 } from "@/lib/form/slides/slide4"
 import { paymentType, recruiterTasks, resumeFormat } from "@/mock/jsons"
 import { getOptionsByArr } from "@/utils/getOptionsByArr"
@@ -7,11 +6,11 @@ import { CheckboxGroup } from "@components/CheckboxGroup/CheckboxGroup"
 import inputStyle from "@components/Input/Input.module.css"
 import { RadioGroup } from "@components/RadioGroup/RadioGroup"
 import RadioGroupStyle from "@components/RadioGroup/RadioGroup.module.css"
+import Arrow from "@public/arrow.svg"
 import clsx from "classnames"
 import { Controller, useFormContext } from "react-hook-form"
-import { Info } from "../../components/Info/Info"
-import Arrow from "@public/arrow.svg"
 import { Error } from "../../Error/Error"
+import { Info } from '../../components/Info/Info';
 const PicturePlaceholder = () => {
   return (
     <div
@@ -41,88 +40,15 @@ export function Slide4() {
   return (
     <FormLayout>
       <h2>Условия сотрудничества</h2>
+
       <div
         style={{
-          marginTop: 28,
-          marginBottom: 110,
           display: "grid",
-          alignItems: "baseline",
-          gridTemplateColumns: "306px max-content 1fr",
-          paddingRight: 190,
+          gridTemplateColumns: "196px 1fr",
+          columnGap: 24,
+          rowGap: 40,
         }}
-        className={globalStyle["marginLeft"]}
       >
-        <div>
-          <PicturePlaceholder />
-          <h3>Установите вознаграждение за сотрудника</h3>
-          <div
-            className={clsx(
-              inputStyle["input-wrapper"],
-              inputStyle["input-wrapper_end-ruble"],
-            )}
-          >
-            <input
-              type="number"
-              placeholder="10000"
-              {...register("employeeReward", { valueAsNumber: true })}
-              className={clsx(inputStyle["input-currency"])}
-            />
-          </div>
-          <Error message={errors.employeeReward?.message} position="relative" />
-
-          <ul
-            style={{
-              margin: "0 0 0 18px",
-              padding: 0,
-              fontSize: 14,
-              lineHeight: "1.4",
-            }}
-          >
-            <li>
-              <p style={{ margin: 0, padding: 0 }}>
-                Рекомендованная сумма — среднемесячный доход кандидата и выше
-              </p>
-            </li>
-            <li>
-              <p style={{ margin: 0, padding: 0 }}>
-                80% суммы — оплата работы рекрутера, 20% — комиссия HRspace
-              </p>
-            </li>
-          </ul>
-        </div>
-        <img
-          src={Arrow}
-          alt="стрелка"
-          style={{ width: 234, height: 113, margin: "auto 25px" }}
-        />
-        <div>
-          <PicturePlaceholder />
-          <h3>Выберите способ выплаты</h3>
-          <div>
-            <Controller
-              name="paymentType"
-              control={control}
-              render={({ field }) => (
-                <RadioGroup
-                  options={getOptionsByArr(paymentType)}
-                  nameGroup="paymentType"
-                  onChange={v => {
-                    field.onChange(v)
-                    trigger("paymentType")
-                  }}
-                  changedValue={field.value}
-                  className={
-                    RadioGroupStyle["radio-group__fieldset_paymentType"]
-                  }
-                />
-              )}
-            />
-            <Error message={errors.paymentType?.message} />
-          </div>
-        </div>
-      </div>
-
-      <div style={{ display: "flex", gap: 20, alignItems: "baseline" }}>
         <h3>Количество сотрудников</h3>
         <div>
           <Controller
@@ -156,56 +82,50 @@ export function Slide4() {
           />
           <Error message={errors.employeeCount?.message} />
         </div>
-      </div>
-      <h3>Что входит в работу рекрутера</h3>
-      <div className={globalStyle["marginLeft"]}>
-        <Controller
-          control={control}
-          name="recruiterTasks"
-          render={({ field }) => {
-            return (
-              <CheckboxGroup
-                options={getOptionsByArr(recruiterTasks)}
-                onChange={field.onChange}
-              />
-            )
-          }}
-        />
-        <Error message={errors.recruiterTasks?.message} />
-      </div>
-      <h3>В каком виде необходимо предоставить резюме кандидатов?</h3>
-      <div className={globalStyle["marginLeft"]}>
-        <Controller
-          name="resumeFormat"
-          control={control}
-          render={({ field }) => (
-            <RadioGroup
-              options={getOptionsByArr(resumeFormat)}
-              nameGroup="resumeFormat"
-              onChange={v => {
-                field.onChange(v)
-                trigger("resumeFormat")
-              }}
-              changedValue={field.value}
-              className={RadioGroupStyle["radio-group__fieldset_resumeFormat"]}
-            />
-          )}
-        />
-        <Error message={errors.resumeFormat?.message} />
-      </div>
-      <div style={{ display: "flex", gap: 40, marginTop: 50 }}>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-          }}
-        >
-          <h3 style={{ margin: 0 }}>Желаемая дата получения первых резюме</h3>
-          <Info style={{ textAlign: "center" }}>(В среднем - от 7 дней. )</Info>
+        <h3>Что входит в работу рекрутера</h3>
+        <div>
+          <Controller
+            control={control}
+            name="recruiterTasks"
+            render={({ field }) => {
+              return (
+                <CheckboxGroup
+                  options={getOptionsByArr(recruiterTasks)}
+                  onChange={field.onChange}
+                />
+              )
+            }}
+          />
+          <Error message={errors.recruiterTasks?.message} />
         </div>
+
+        <h3>В каком виде необходимо предоставить резюме кандидатов?</h3>
+        <div>
+          <Controller
+            name="resumeFormat"
+            control={control}
+            render={({ field }) => (
+              <RadioGroup
+                options={getOptionsByArr(resumeFormat)}
+                nameGroup="resumeFormat"
+                onChange={v => {
+                  field.onChange(v)
+                  trigger("resumeFormat")
+                }}
+                changedValue={field.value}
+                className={
+                  RadioGroupStyle["radio-group__fieldset_resumeFormat"]
+                }
+              />
+            )}
+          />
+          <Error message={errors.resumeFormat?.message} />
+        </div>
+
+        <h3 style={{ margin: 0 }}>Желаемая дата получения первых резюме</h3>
         <div>
           <input
+            style={{ width: 200 }}
             type="date"
             className={inputStyle["input-currency"]}
             {...register("dates.desiredFirstResumeDate", {
@@ -214,25 +134,104 @@ export function Slide4() {
           />
           <Error message={errors.dates?.desiredFirstResumeDate?.message} />
         </div>
-      </div>
-      <div style={{ display: "flex", gap: 20, marginTop: 24 }}>
-        <div>
-          <h3 style={{ margin: 0 }}>
-            Желаемая дата выхода сотрудника на работу
-          </h3>
-          <Info style={{ textAlign: "center" }}>
-            По статистике площадки средний срок - 1 месяц.
-          </Info>
-        </div>
+        <h3 style={{ margin: 0 }}>Желаемая дата выхода сотрудника на работу</h3>
         <div>
           <input
             className={inputStyle["input-currency"]}
+            style={{ width: 200 }}
             type="date"
             {...register("dates.desiredEmployeeExitDate", {
               setValueAs: v => new Date(v).getTime(),
             })}
           />
+          <Info>
+            За срочный поиск рекрутер может запросить более высокую цену.
+          </Info>
           <Error message={errors.dates?.desiredEmployeeExitDate?.message} />
+        </div>
+      </div>
+
+      <div
+        style={{
+          marginTop: 48,
+          marginBottom: 110,
+          display: "grid",
+          alignItems: "baseline",
+          gridTemplateColumns: "1fr max-content 1fr",
+        }}
+      >
+        <div>
+          <h2>Сумма вознаграждения рекрутеру</h2>
+          <div style={{ display: "flex", gap: 8 }}>
+            <div
+              className={clsx(
+                inputStyle["input-wrapper"],
+                inputStyle["input-wrapper_end-ruble"],
+              )}
+            >
+              <input
+                type="number"
+                placeholder="10000"
+                {...register("employeeReward", { valueAsNumber: true })}
+                className={clsx(inputStyle["input-currency"])}
+              />
+            </div>
+            <p>/за одного сотрудника</p>
+          </div>
+            <Info>Это средняя сумма по рынку для этой специальности  </Info>
+          <Error message={errors.employeeReward?.message} position="relative" />
+
+          <ul
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              margin: "12px 0 0 18px",
+              padding: "0 0 0 4px",
+              fontSize: 14,
+              lineHeight: "1.4",
+              gap: 12
+            }}
+          >
+            <li>
+              <p style={{ margin: 0, padding: 0 }}>
+                Рекомендованная сумма — среднемесячный доход кандидата и выше
+              </p>
+            </li>
+            <li>
+              <p style={{ margin: 0, padding: 0 }}>
+                80% суммы — оплата работы рекрутера, 20% — комиссия HRspace
+              </p>
+            </li>
+          </ul>
+        </div>
+        <img
+          src={Arrow}
+          alt="стрелка"
+          style={{ width: 234, height: 113, margin: "auto 25px" }}
+        />
+        <div>
+          <h2>Выберите способ выплаты</h2>
+          <div>
+            <Controller
+              name="paymentType"
+              control={control}
+              render={({ field }) => (
+                <RadioGroup
+                  options={getOptionsByArr(paymentType)}
+                  nameGroup="paymentType"
+                  onChange={v => {
+                    field.onChange(v)
+                    trigger("paymentType")
+                  }}
+                  changedValue={field.value}
+                  className={
+                    RadioGroupStyle["radio-group__fieldset_paymentType"]
+                  }
+                />
+              )}
+            />
+            <Error message={errors.paymentType?.message} />
+          </div>
         </div>
       </div>
     </FormLayout>
